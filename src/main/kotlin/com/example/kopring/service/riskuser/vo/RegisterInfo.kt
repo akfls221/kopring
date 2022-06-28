6cc4f08b8ea7b21c97b6de5faf3547ebe56edd35
엄태권 <akfls221@gmail.com>
2022-06-29 02:11:03 +0900

    kopring - RiskUserVo 생성

---
+++ src/main/kotlin/com/example/kopring/service/riskuser/vo/RegisterInfo.kt
@@ -0,0 +1,13 @@
+package com.example.kopring.service.riskuser.vo
+
+class RegisterInfo (
+    val accNo: String? = null,
+    val email: String? = null,
+    val firstName: String? = null,
+    val lastName: String? = null,
+    val localName: String? = null,
+    val bsb: String? = null,
+    val bank: String? = null,
+    val mobile: String? = null,
+    val reason: String? = null,
+)

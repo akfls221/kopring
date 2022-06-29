518d302beffb0d71b453e8ea5d7a513c766e2e90
엄태권 <akfls221@gmail.com>
2022-06-30 02:56:27 +0900

    kopring - @EnableJpaAuditing 추가

--- src/main/kotlin/com/example/kopring/KopringApplication.kt
+++ src/main/kotlin/com/example/kopring/KopringApplication.kt
@@ -2,8 +2,10 @@ package com.example.kopring
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class KopringApplication
 
 fun main(args: Array<String>) {

6e51460cd5b9aa62d6e667981281b5a685f1f5a8
엄태권 <akfls221@gmail.com>
2022-06-30 02:57:17 +0900

    kopring - RiskUserController-add api 추가

--- src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
+++ src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
@@ -1,20 +1,22 @@
 package com.example.kopring.controller
 
-import com.example.kopring.data.RiskUserData
-import com.example.kopring.domain.RiskUser
+import com.example.kopring.controller.data.RiskUserData
+import com.example.kopring.controller.data.RiskUserInfo
+import com.example.kopring.service.riskuser.implement.RiskUserAddService
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-class RiskUserController {
+class RiskUserController(
+    private val riskUserAddService: RiskUserAddService,
+){
 
     @PostMapping("/api/add")
-    fun addRiskUser(@RequestBody request: RiskUserData.RegisterRequest) : RiskUserData.RegisterResponse {
-        print(request)
-        return RiskUserData.RegisterResponse(1, null)
+    fun addRiskUser(@RequestBody request: RiskUserInfo.RegisterRequest) : ResponseEntity<RiskUserInfo.RegisterResponse> {
+        return ResponseEntity.ok(riskUserAddService.register(request.toRegisterInfo()))
     }
 }
 

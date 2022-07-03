2734b7b92a45806661ed03c24fa6435073dd58f7
엄태권 <akfls221@gmail.com>
2022-07-04 03:14:41 +0900

    kopring - spring validate 추가

--- src/main/kotlin/com/example/kopring/controller/data/RiskUserData.kt
+++ src/main/kotlin/com/example/kopring/controller/data/RiskUserData.kt
@@ -2,13 +2,17 @@ package com.example.kopring.controller.data
 
 import com.example.kopring.service.riskuser.vo.RegisterInfo
 import java.time.LocalDateTime
+import javax.validation.constraints.Email
+import javax.validation.constraints.NotBlank
 
 class RiskUserData {
 }
 
 class RiskUserInfo{
     data class RegisterRequest(
-        val accNo: String? = null,
+        @field:NotBlank(message = "비어 있는 값일 수 없습니다.")
+        val accNo: String?,
+        @field:Email(message = "올바른 이메일 형식이 아닙니다.")
         val email: String? = null,
         val firstName: String? = null,
         val lastName: String? = null,

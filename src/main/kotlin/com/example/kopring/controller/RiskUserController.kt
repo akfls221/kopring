f52d8cb5e888f20b0e16477c243ee87f13db3d69
엄태권 <akfls221@gmail.com>
2022-07-08 01:06:15 +0900

    kopring - 조회시 Controller 추가

--- src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
+++ src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
@@ -1,8 +1,12 @@
 package com.example.kopring.controller
 
+import com.example.kopring.controller.data.RiskUserData
 import com.example.kopring.controller.data.RiskUserInfo
+import com.example.kopring.service.riskuser.RiskUserQueryService
 import com.example.kopring.service.riskuser.implement.RiskUserAddService
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
@@ -11,11 +15,17 @@ import javax.validation.Valid
 @RestController
 class RiskUserController(
     private val riskUserAddService: RiskUserAddService,
-){
+    private val riskUserQueryService: RiskUserQueryService
+) {
 
     @PostMapping("/api/add")
-    fun addRiskUser(@RequestBody @Valid request: RiskUserInfo.RegisterRequest) : ResponseEntity<RiskUserInfo.RegisterResponse> {
+    fun addRiskUser(@RequestBody @Valid request: RiskUserInfo.RegisterRequest): ResponseEntity<RiskUserInfo.RegisterResponse> {
         return ResponseEntity.ok(riskUserAddService.register(request.toRegisterInfo()))
+    }
+
+    @GetMapping("/api/{id}")
+    fun detailRiskUser(@PathVariable id : Long): RiskUserData.FormData {
+        return riskUserQueryService.getRiskUserById(id)
     }
 }
 

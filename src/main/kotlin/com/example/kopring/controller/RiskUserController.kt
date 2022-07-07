1945772fc122104a44d6e9f8916d8e8411d44e2d
엄태권 <akfls221@gmail.com>
2022-07-08 00:17:47 +0900

    fix : kopring - spring validate 어노테이션 추가

--- src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
+++ src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
@@ -1,13 +1,12 @@
 package com.example.kopring.controller
 
-import com.example.kopring.controller.data.RiskUserData
 import com.example.kopring.controller.data.RiskUserInfo
 import com.example.kopring.service.riskuser.implement.RiskUserAddService
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 class RiskUserController(
@@ -15,7 +14,7 @@ class RiskUserController(
 ){
 
     @PostMapping("/api/add")
-    fun addRiskUser(@RequestBody request: RiskUserInfo.RegisterRequest) : ResponseEntity<RiskUserInfo.RegisterResponse> {
+    fun addRiskUser(@RequestBody @Valid request: RiskUserInfo.RegisterRequest) : ResponseEntity<RiskUserInfo.RegisterResponse> {
         return ResponseEntity.ok(riskUserAddService.register(request.toRegisterInfo()))
     }
 }

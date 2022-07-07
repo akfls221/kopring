37a83b52396edc21666404b7acadf9148bb7b7dd
엄태권 <akfls221@gmail.com>
2022-07-08 01:06:34 +0900

    kopring - Exception 재정의

--- src/main/kotlin/com/example/kopring/exception/TkException.kt
+++ src/main/kotlin/com/example/kopring/exception/TkException.kt
@@ -1,12 +1,14 @@
 package com.example.kopring.exception
 
+import org.springframework.http.HttpStatus
+
 data class ResponseError(
     val code: Int? = null,
     val dec: String? = null
 )
 
 class TkException(
+    val status: HttpStatus,
     val code: Int? = null,
-    val dec: String? = null,
     override val message: String? = null
 ) : RuntimeException()

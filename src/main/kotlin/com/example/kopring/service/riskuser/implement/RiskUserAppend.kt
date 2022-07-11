21d7bde0606b2d8028fb1c4affa9ebd3725be16c
엄태권 <akfls221@gmail.com>
2022-07-12 02:52:48 +0900

    kopring - 수정을 위한 modify 메서드 추가

--- src/main/kotlin/com/example/kopring/service/riskuser/implement/RiskUserAppend.kt
+++ src/main/kotlin/com/example/kopring/service/riskuser/implement/RiskUserAppend.kt
@@ -1,10 +1,14 @@
 package com.example.kopring.service.riskuser.implement
 
-import com.example.kopring.controller.data.RiskUserInfo
+
 import com.example.kopring.domain.RiskUser
 import com.example.kopring.exception.TkException
+import com.example.kopring.exception.enum.Status
 import com.example.kopring.repository.RiskUserRepository
+import com.example.kopring.service.riskuser.vo.ModifyInfo
 import com.example.kopring.service.riskuser.vo.RegisterInfo
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
@@ -18,5 +22,15 @@ class RiskUserAppend (
 
     fun add(registerInfo: RegisterInfo) : RiskUser {
         return riskUserRepository.save(entityMapper.convertToEntity(registerInfo))
+    }
+
+    fun update(id: Long, modifyInfo: ModifyInfo): RiskUser {
+        val riskUser = riskUserRepository.findByIdOrNull(id) ?: throw TkException(
+            HttpStatus.BAD_REQUEST,
+            Status.NoExistEx.code,
+            Status.NoExistEx.dec
+        )
+        riskUser.modify(modifyInfo)
+        return riskUserRepository.save(riskUser)
     }
 }

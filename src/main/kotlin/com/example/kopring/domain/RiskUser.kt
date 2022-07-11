96e5eeba4e488a58377dcdfd4300687694f1a3de
엄태권 <akfls221@gmail.com>
2022-07-12 02:52:03 +0900

    kopring - 수정을 위한 데이터 request 추가

--- src/main/kotlin/com/example/kopring/domain/RiskUser.kt
+++ src/main/kotlin/com/example/kopring/domain/RiskUser.kt
@@ -2,6 +2,7 @@ package com.example.kopring.domain
 
 import com.example.kopring.domain.core.AbstractEntity
 import com.example.kopring.domain.core.AuditEntity
+import com.example.kopring.service.riskuser.vo.ModifyInfo
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 import javax.persistence.Column
 import javax.persistence.Entity
@@ -15,26 +16,38 @@ class RiskUser (
     @Id
     @GeneratedValue
     @Column(name = "riskuser_id")
-    val id: Long? = null,
+    var id: Long? = null,
 
-    val accNo: String? = null,
+    var accNo: String? = null,
 
-    val email: String? = null,
+    var email: String? = null,
 
-    val firstName: String? = null,
+    var firstName: String? = null,
 
-    val lastName: String? = null,
+    var lastName: String? = null,
 
-    val localName: String? = null,
+    var localName: String? = null,
 
-    val bsb: String? = null,
+    var bsb: String? = null,
 
-    val bank: String? = null,
+    var bank: String? = null,
 
-    val mobile: String? = null,
+    var mobile: String? = null,
 
-    val reason: String? = null,
+    var reason: String? = null,
 
 ) : AuditEntity() {
+
+    fun modify(modifyInfo: ModifyInfo) {
+        modifyInfo.accNo?.let { accNo = modifyInfo.accNo }
+        modifyInfo.email?.let { email = modifyInfo.email }
+        modifyInfo.firstName?.let { firstName = modifyInfo.firstName }
+        modifyInfo.localName?.let { localName = modifyInfo.localName }
+        modifyInfo.bsb?.let { bsb = modifyInfo.bsb }
+        modifyInfo.bank?.let { bank = modifyInfo.bank }
+        modifyInfo.mobile?.let { mobile = modifyInfo.mobile }
+        modifyInfo.reason?.let { reason = modifyInfo.reason }
+
+    }
 
 }

32c0039553c59f63556c5a2e6d038081ef7a5784
엄태권 <akfls221@gmail.com>
2022-07-12 02:52:21 +0900

    kopring - 수정을 위한 데이터 request 추가

--- src/main/kotlin/com/example/kopring/controller/data/RiskUserData.kt
+++ src/main/kotlin/com/example/kopring/controller/data/RiskUserData.kt
@@ -1,5 +1,6 @@
 package com.example.kopring.controller.data
 
+import com.example.kopring.service.riskuser.vo.ModifyInfo
 import com.example.kopring.service.riskuser.vo.RegisterInfo
 import java.time.LocalDateTime
 import javax.validation.constraints.Email
@@ -49,8 +50,39 @@ class RiskUserInfo{
         )
     }
 
+    data class ModifyRequest(
+        @field:NotBlank(message = "비어 있는 값일 수 없습니다.")
+        val accNo: String?,
+        @field:Email(message = "올바른 이메일 형식이 아닙니다.")
+        val email: String? = null,
+        val firstName: String? = null,
+        val lastName: String? = null,
+        val localName: String? = null,
+        val bsb: String? = null,
+        val bank: String? = null,
+        val mobile: String? = null,
+        val reason: String? = null,
+    ){
+        fun toModifyInfo() = ModifyInfo(
+            accNo = accNo,
+            email = email,
+            firstName = firstName,
+            lastName = lastName,
+            localName = localName,
+            bsb = bsb,
+            bank = bank,
+            mobile = mobile,
+            reason = reason
+        )
+    }
+
     data class RegisterResponse(
         val registId : Long?,
         val dateTime: LocalDateTime? = null
+    )
+
+    data class ModifyResponse(
+        val registId : Long?,
+        val modifyDate: LocalDateTime? = null
     )
 }

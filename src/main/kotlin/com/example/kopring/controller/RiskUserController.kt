8a163ebb3abee6acfb06008d637fad219dfddc36
엄태권 <akfls221@gmail.com>
2022-07-12 02:53:59 +0900

    kopring - 수정을 위한 modify controller 추가

--- src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
+++ src/main/kotlin/com/example/kopring/controller/RiskUserController.kt
@@ -2,12 +2,14 @@ package com.example.kopring.controller
 
 import com.example.kopring.controller.data.RiskUserData
 import com.example.kopring.controller.data.RiskUserInfo
+import com.example.kopring.service.riskuser.RiskUserModifyService
 import com.example.kopring.service.riskuser.RiskUserQueryService
 import com.example.kopring.service.riskuser.implement.RiskUserAddService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import javax.validation.Valid
@@ -15,14 +17,27 @@ import javax.validation.Valid
 @RestController
 class RiskUserController(
     private val riskUserAddService: RiskUserAddService,
+    private val riskUserModifyService: RiskUserModifyService,
     private val riskUserQueryService: RiskUserQueryService
 ) {
 
+    /**
+     * 블랙리스트 유저 ADD
+     */
     @PostMapping("/api/add")
     fun addRiskUser(@RequestBody @Valid request: RiskUserInfo.RegisterRequest): ResponseEntity<RiskUserInfo.RegisterResponse> {
         return ResponseEntity.ok(riskUserAddService.register(request.toRegisterInfo()))
     }
 
+    @PostMapping("/api/{id}")
+    fun editRiskUser(@PathVariable id: Long, @RequestBody @Valid request: RiskUserInfo.ModifyRequest): ResponseEntity<RiskUserInfo.ModifyResponse> {
+        return ResponseEntity.ok(riskUserModifyService.modify(id, request.toModifyInfo()))
+
+    }
+
+    /**
+     * 블랙리스트 상세정보 GET
+     */
     @GetMapping("/api/{id}")
     fun detailRiskUser(@PathVariable id : Long): RiskUserData.FormData {
         return riskUserQueryService.getRiskUserById(id)

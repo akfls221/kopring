3a5199cde03eaafb9573c5015f14c5bc140f852f
엄태권 <akfls221@gmail.com>
2022-07-18 02:23:38 +0900

    kopring - 삭제를 위한 RiskUserDelete 추가

---
+++ src/main/kotlin/com/example/kopring/service/riskuser/implement/RiskUserDelete.kt
@@ -0,0 +1,28 @@
+package com.example.kopring.service.riskuser.implement
+
+import com.example.kopring.controller.data.RiskUserInfo
+import com.example.kopring.exception.TkException
+import com.example.kopring.exception.enum.Status
+import com.example.kopring.repository.RiskUserRepository
+import org.springframework.http.HttpStatus
+import org.springframework.stereotype.Component
+import org.springframework.transaction.annotation.Transactional
+
+@Component
+@Transactional
+class RiskUserDelete(
+    private val riskUserRepository: RiskUserRepository,
+    private val entityMapper: EntityMapper
+) {
+
+    fun deleteUser(id: Long) : RiskUserInfo.DeleteResponse{
+        val riskUser = riskUserRepository.findById(id).orElseThrow{ throw TkException(
+            HttpStatus.BAD_REQUEST,
+            Status.NoExistEx.code,
+            Status.NoExistEx.dec
+        )}
+        riskUserRepository.deleteById(id)
+        return entityMapper.converToRiskUserDeleteResponse(riskUser)
+    }
+
+}

e7e4d7aa797b5dbd0090ca8f55227e5dd0951293
엄태권 <akfls221@gmail.com>
2022-07-20 03:01:54 +0900

    fix : kopring - 설명주석 추가

--- src/main/kotlin/com/example/kopring/repository/RiskUserRepository.kt
+++ src/main/kotlin/com/example/kopring/repository/RiskUserRepository.kt
@@ -7,6 +7,9 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
 
 interface RiskUserRepository : JpaRepository<RiskUser, Long>{
+    /**
+     * id를 desc로 page 리스트 조회
+     */
     @Query("select r from RiskUser r order by r.id desc ")
     fun findAllBasic(pageable: Pageable): Page<RiskUser>
 

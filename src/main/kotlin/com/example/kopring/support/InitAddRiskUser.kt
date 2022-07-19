74d2c5839c69681d0dad9d8621d0a265bfadd694
엄태권 <akfls221@gmail.com>
2022-07-20 01:38:21 +0900

    kopring - 페이징 코드를 위한 InitializingBean 상속을 통한 init save

---
+++ src/main/kotlin/com/example/kopring/support/InitAddRiskUser.kt
@@ -0,0 +1,33 @@
+package com.example.kopring.support
+
+import com.example.kopring.repository.RiskUserRepository
+import com.example.kopring.service.riskuser.implement.EntityMapper
+import com.example.kopring.service.riskuser.vo.RegisterInfo
+import org.springframework.beans.factory.InitializingBean
+import org.springframework.stereotype.Component
+
+@Component
+class InitAddRiskUser(
+    private val riskUserRepository : RiskUserRepository,
+    private val entityMapper: EntityMapper
+): InitializingBean{
+    override fun afterPropertiesSet() {
+        for (i in 1..100) {
+            riskUserRepository.save(
+                entityMapper.convertToEntity(
+                    RegisterInfo(
+                        accNo = "$i",
+                        email = "akfls22$i@gmail.com",
+                        reason = "init save$i",
+                        mobile = "010-6242-694$i",
+                        bank = "kakao$i",
+                        bsb = "bsb$i",
+                        localName = "owen$i",
+                        lastName = "um$i",
+                        firstName = "$i"
+                    )
+                )
+            )
+        }
+    }
+}

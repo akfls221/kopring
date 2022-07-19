19cbf127420750c3f2e344e35f5828b607a2ec0b
엄태권 <akfls221@gmail.com>
2022-07-20 02:55:35 +0900

    kopring - 페이징 코드를 위한 Repository 쿼리 생성

--- src/main/kotlin/com/example/kopring/repository/RiskUserRepository.kt
+++ src/main/kotlin/com/example/kopring/repository/RiskUserRepository.kt
@@ -1,7 +1,13 @@
 package com.example.kopring.repository
 
 import com.example.kopring.domain.RiskUser
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface RiskUserRepository : JpaRepository<RiskUser, Long>{
+    @Query("select r from RiskUser r order by r.id desc ")
+    fun findAllBasic(pageable: Pageable): Page<RiskUser>
+
 }

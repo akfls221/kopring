b7677393168b5239da57c101cacb85bcb66f86fd
엄태권 <akfls221@gmail.com>
2022-07-20 02:56:14 +0900

    kopring - 페이징 코드를 위한 Service fun 추가구현

--- src/main/kotlin/com/example/kopring/service/riskuser/RiskUserQueryService.kt
+++ src/main/kotlin/com/example/kopring/service/riskuser/RiskUserQueryService.kt
@@ -1,11 +1,12 @@
 package com.example.kopring.service.riskuser
 
 import com.example.kopring.controller.data.RiskUserData
-import com.example.kopring.exception.TkException
-import com.example.kopring.exception.enum.Status
-import com.example.kopring.repository.RiskUserRepository
+import com.example.kopring.domain.RiskUser
 import com.example.kopring.service.riskuser.implement.EntityMapper
 import com.example.kopring.service.riskuser.implement.RiskUserReader
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.util.*
@@ -14,8 +15,9 @@ interface RiskUserQueryService {
     /**
      * 블랙리스트 유저 조회
      */
-
     fun getRiskUserById(id: Long) : RiskUserData.FormData
+
+    fun getTotalRiskUser(pageable: Pageable) : Page<RiskUserData.FormData>
 
 }
 
@@ -31,5 +33,11 @@ class RiskUserQueryServiceImpl(
         return entityMapper.convertToRiskUserInfoToRiskUserFormData(readRiskUser)
     }
 
+    override fun getTotalRiskUser(pageable: Pageable): Page<RiskUserData.FormData> {
+        val page = if (pageable.pageNumber == 0) 0 else (pageable.pageNumber - 1)
+        val pageRequest = PageRequest.of(page, 10)
+        val totalRiskUser: Page<RiskUser> = riskUserReader.getTotalRiskUser(pageRequest)
+        return totalRiskUser.map { entityMapper.convertToRiskUserInfoToRiskUserFormData(it) }
+    }
 }
 

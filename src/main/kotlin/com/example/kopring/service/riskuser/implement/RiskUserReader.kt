4a64463798ac0cad12456ce3666f6029cd4db307
엄태권 <akfls221@gmail.com>
2022-07-20 02:55:58 +0900

    kopring - 페이징 코드를 위한 Reader function 생성

--- src/main/kotlin/com/example/kopring/service/riskuser/implement/RiskUserReader.kt
+++ src/main/kotlin/com/example/kopring/service/riskuser/implement/RiskUserReader.kt
@@ -3,6 +3,8 @@ package com.example.kopring.service.riskuser.implement
 import com.example.kopring.domain.RiskUser
 import com.example.kopring.exception.TkException
 import com.example.kopring.repository.RiskUserRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
@@ -18,5 +20,15 @@ class RiskUserReader(
                 code = HttpStatus.BAD_REQUEST.value(),
                 message = "존재하지 않는 블랙리스트 입니다."
             )
+    }
+
+    fun getTotalRiskUser(pageable: Pageable) : Page<RiskUser>{
+        riskUserRepository.findAllBasic(pageable).let {
+            if(it.size <= 0) throw TkException(
+                status = HttpStatus.BAD_REQUEST,
+                code = HttpStatus.BAD_REQUEST.value(),
+                message = "존재하지 않는 블랙리스트 입니다."
+            ) else return riskUserRepository.findAllBasic(pageable)
+        }
     }
 }
